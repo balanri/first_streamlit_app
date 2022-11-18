d5dcf16959296dc1474ac7dc1e9b5a177999db63
Balaji <118508881+balanri@users.noreply.github.com>
2022-11-18 12:54:42 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,3 @@ streamlit.header('Breakfast menu')
 streamlit.text  ('Omega 3 & Blueberry oatmeal')
 streamlit.text  ('Kale , Spinach & Rocket Smoothie')
 streamlit.text  ('Hard-Boiled Free-Range Egg')
-
-
